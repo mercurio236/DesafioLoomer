eccead76658aa1b87aa819c936556eb5b978a890
Arley Souto <macx15@hotmail.com>
2021-08-25 17:45:53 -0300

    Rotas

---
+++ desafio/src/routers.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'
+
+const Stack = createNativeStackNavigator();
+
+import Home from './Pages/Home';
+import Favorito from './Pages/Favorito';
+
+export default function Routers() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Favorito" component={Favorito}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
